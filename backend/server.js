765140e0e831d9cf76447485f841b79fe97178d6
Ase-Fullstack <asefullstack@egmail.com>
2023-12-20 17:08:26 +0300

    Edit server.js and package.json

--- backend/server.js
+++ backend/server.js
@@ -18,13 +18,21 @@ mongoose
     console.log(err.message);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/api/keys/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
 });
 
 app.use('/api/seed', seedRouter);
@@ -32,17 +40,11 @@ app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
-const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
-});
-
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
-  console.log(`serve at http://localhost:${port}`);
 });
